96a31bbc8194e2da6b5dd2d091f487da251710b6
mattew <mattew1997@yandex.ru>
2025-01-11 12:44:29 +0300

    Some fix

--- src/main/java/dev/rest_api/cinema/entity/Movie.java
+++ src/main/java/dev/rest_api/cinema/entity/Movie.java
@@ -1,6 +1,7 @@
 package dev.rest_api.cinema.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import java.util.List;
 
 @Document(collection = "movies")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
@@ -25,76 +27,4 @@ public class Movie {
     @DocumentReference //Ссылка на документ reviews в MongoDB
     private List<Review> reviewsIds;
 
-
-    public Object getId() {
-        return id;
-    }
-
-    public String getImdbId() {
-        return imdbId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getReleaseDate() {
-        return releaseDate;
-    }
-
-    public String getTrailerLink() {
-        return trailerLink;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public List<String> getGenres() {
-        return genres;
-    }
-
-    public List<String> getBackdrops() {
-        return backdrops;
-    }
-
-    public List<Review> getReviewsIds() {
-        return reviewsIds;
-    }
-
-    public void setId(Object id) {
-        this.id = id;
-    }
-
-    public void setImdbId(String imdbId) {
-        this.imdbId = imdbId;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public void setTrailerLink(String trailerLink) {
-        this.trailerLink = trailerLink;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
-    public void setGenres(List<String> genres) {
-        this.genres = genres;
-    }
-
-    public void setBackdrops(List<String> backdrops) {
-        this.backdrops = backdrops;
-    }
-
-    public void setReviewsIds(List<Review> reviewsIds) {
-        this.reviewsIds = reviewsIds;
-    }
 }
--- src/main/java/dev/rest_api/cinema/entity/Review.java
+++ src/main/java/dev/rest_api/cinema/entity/Review.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "reviews")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
@@ -16,22 +17,6 @@ public class Review {
     private String reviewsBody;
 
     public Review(String reviewsBody) {
-        this.reviewsBody = reviewsBody;
-    }
-
-    public Object getId() {
-        return id;
-    }
-
-    public void setId(Object id) {
-        this.id = id;
-    }
-
-    public String getReviewsBody() {
-        return reviewsBody;
-    }
-
-    public void setReviewsBody(String reviewsBody) {
         this.reviewsBody = reviewsBody;
     }
 }
